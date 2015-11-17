62771b53897a08469a6ed85a02ae4b21c6eb6d15
CactusTribe <lefrancjoaquim@gmail.com>
2015-11-17 16:59:11 +0100

    Lecture de la premiere ligne d'un fichier

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,16 @@
-let printHello () = 
-	print_string("Hello world !");
-	print_newline();;
+let fichier = "automate.txt";;
 
-printHello();;
+let print_file file =
+	let ic = open_in file in
+	try 
+		let line = input_line ic in
+		print_endline line;
+		flush stdout;
+		close_in ic
+
+	with e ->
+		close_in_noerr ic;
+		raise e
+;;
+
+print_file(fichier);
