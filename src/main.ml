855c175a401ada5dae4d93c78d89dab5b57052a0
CactusTribe <lefrancjoaquim@gmail.com>
2015-11-18 12:27:02 +0100

    Quelques modifications, fonction parse en cours d'écriture

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,36 @@
+open String
+
+(* Définition des types *)
+type state = ALIVE | DEAD;;
+
+(* Ouverture du fichier *)
 let fichier = "automate.txt";;
 let in_chanel = open_in fichier;;
 
+(* Informations du fichier *)
+let sizeGrid = ref 0;;
+
+(* Fonctions de parsing *)
+let parse in_chanel = 
+	try
+		(* Récupération de la taille *)
+		let line = input_line in_chanel in
+			sizeGrid := int_of_string line;
+
+		(* Récupération des règles *)
+		let line = input_line in_chanel in
+			if (String.compare line "Regles") = 0 then 
+				begin 
+					let line = input_line in_chanel in
+						print_endline line;
+			end else print_endline "Fichier malformé.";
+
+	with 
+	| End_of_file -> print_string("Fin du fichier.\n");
+	| e -> close_in_noerr in_chanel; raise e;
+;;
+
+(*
 let rec print_file () = 
 	try
 		let line = input_line in_chanel in
@@ -10,9 +40,11 @@ let rec print_file () =
 
 	with 
 	| End_of_file -> print_string("Fin du fichier.\n");
-	| e -> close_in_noerr in_chanel;
-				 raise e;
-;;	
+	| e -> close_in_noerr in_chanel; raise e;
+;;*)	
 
+parse in_chanel;;
 
-print_file ();
+print_int !sizeGrid;;
+print_newline();;
+
