ea893de96246e8f071b16816990e69c6cff5160d
CactusTribe <lefrancjoaquim@gmail.com>
2015-11-21 18:47:17 +0100

    Essai de listes

--- src/main.ml
+++ src/main.ml
@@ -1,24 +1,35 @@
 open String
 
+(* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= *)
 (* Définition des exceptions *)
 exception File_structure
+(* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= *)
 
+(* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= *)
 (* Définition des types *)
 type state = A | D ;;
 type generation = state array array;;
 type rule = string ;;
 type automaton = rule list;;
+(* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= *)
 
 (* Ouverture du fichier *)
 let fichier = "automate.txt";;
 let in_chanel = open_in fichier;;
 
-(* Fonctions de parsing *)
+(* Affiche une liste *)
+let rec print_list = function
+	|[] -> print_newline()
+	| a::t -> print_string a; print_string " "; print_list t
+;;
+
+(* Récupère la première ligne du fichier *)
 let getSizeGrid in_chanel = 
 	let line = input_line in_chanel in
 		int_of_string line
 ;;
 
+(* Récupère les règles du fichier *)
 let rec getRules in_chanel =
 	let line = input_line in_chanel in
 		if (String.compare line "GenerationZero") != 0 then
@@ -29,12 +40,14 @@ let rec getRules in_chanel =
 		else ()
 ;;
 
+(* Récupère la génération Zero *)
 let rec getGenerationZero in_chanel = 
 	let line = input_line in_chanel in
 		print_endline line;
 		getGenerationZero in_chanel;
 ;;
 
+(* Construit l'automate contenu dans le fichier *)
 let parse in_chanel = 
 	try
 		(* Récupération de la taille *)
@@ -42,10 +55,11 @@ let parse in_chanel =
 		print_newline();
 
 		(* Récupération des règles *)
+
 		let line = input_line in_chanel in
-		if (String.compare line "Regles") = 0 then
-			getRules in_chanel
-		else raise File_structure;
+			if (String.compare line "Regles") = 0 then
+				getRules in_chanel
+			else raise File_structure;
 
 		(* Récupération de la generationZero *)
 		getGenerationZero in_chanel;
@@ -56,6 +70,11 @@ let parse in_chanel =
 	| e -> close_in_noerr in_chanel; raise e;
 ;;
 
-parse in_chanel;;
+let rule1 : rule = "BBBCB";;
+let rule2 : rule = "CCBCB";;
 
-(* Mon nouveau commentaire super utile *)
+let maListe : rule list = rule1::rule2::[];;
+
+print_list maListe;
+
+parse in_chanel;;
