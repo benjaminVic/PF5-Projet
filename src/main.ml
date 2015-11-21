df9fa6f48c0744605a48c5d078a7c357af38b174
CactusTribe <lefrancjoaquim@gmail.com>
2015-11-21 14:20:18 +0100

    Modification de la structure du parsing

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,8 @@
 open String
 
+(* Définition des exceptions *)
+exception File_structure
+
 (* Définition des types *)
 type state = A | D ;;
 type generation = state array array;;
@@ -10,32 +13,33 @@ type automaton = rule list;;
 let fichier = "automate.txt";;
 let in_chanel = open_in fichier;;
 
-(* Informations du fichier *)
-let sizeGrid = ref 0;;
-
 (* Fonctions de parsing *)
+let getSizeGrid in_chanel = 
+	let line = input_line in_chanel in
+		int_of_string line
+;;
+
+let getRules in_chanel = 
+	let line = input_line in_chanel in
+		print_endline line;
+;;
+
 let parse in_chanel = 
 	try
 		(* Récupération de la taille *)
-		let line = input_line in_chanel in
-			sizeGrid := int_of_string line;
+		print_int (getSizeGrid in_chanel);
+		print_newline();
 
 		(* Récupération des règles *)
 		let line = input_line in_chanel in
-			if (String.compare line "Regles") = 0 then 
-				begin 
-					let line = input_line in_chanel in
-						print_endline line;
-			end else print_endline "Fichier malformé.";
-
+		if (String.compare line "Regles") = 0 then
+			getRules in_chanel
+		else raise File_structure;
+		
 	with 
 	| End_of_file -> print_string("Fin du fichier.\n");
 	| e -> close_in_noerr in_chanel; raise e;
 ;;
 
-
 parse in_chanel;;
-
-print_int !sizeGrid;;
-print_newline();;
 
