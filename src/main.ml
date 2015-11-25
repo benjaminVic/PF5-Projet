f7288ae9288192cb51e6f3da34a4697982980188
CactusTribe <lefrancjoaquim@gmail.com>
2015-11-25 17:03:35 +0100

    Ajout des fonctions next_state et next_generation

--- src/main.ml
+++ src/main.ml
@@ -69,36 +69,22 @@ let parse in_chanel =
 (* SANS PARSING DE FICHIER *)
 (* ############################################## *)
 
-let rule1 = (A,A,A,A,A);;
-let rule2 = (A,A,A,A,D);;
-let rule3 = (A,A,A,D,A);;
-let rule4 = (A,A,A,D,D);;
-
 let sizeGrid = 7;;
-let automaton = rule1::rule2::rule3::rule4::[];;
 
-let generationZero = Array.make_matrix 7 7 A;; 
-generationZero.(0).(0) <- D;;
-generationZero.(1).(1) <- D;;
-generationZero.(2).(2) <- D;;
-generationZero.(3).(3) <- D;;
-generationZero.(4).(4) <- D;;
-generationZero.(5).(5) <- D;;
-generationZero.(6).(6) <- D;;
+(* Modifie une cellule x;y dans une génération g avec l'état s *)
+let setState g x y s = 
+	g.(x).(y) <- s;
+;;
 
+(* Affiche un etat s *)
 let print_state s = match s with
-| A -> print_string "A"
-| D -> print_string "D"
+| A -> print_string " A "
+| D -> print_string " D "
 ;;
 
-let rec next_state listesRules(n,e,s,o,cell) = match listesRules with
-  | [] -> cell
-  | a::t -> match a with
-    | (a,b,c,d,f) when (a=n && b=e && c=s && d=o && f=cell) -> if cell = A then D else A
-    | _ -> next_state t  (n,e,s,o,cell)
-;;
-
-let show_generation g = 
+(* Affiche une génération g *)
+let show_generation g =
+	print_endline "---------------------";
 	for i = 0 to sizeGrid-1 do
 	begin
 		for j = 0 to sizeGrid-1 do
@@ -106,7 +92,57 @@ let show_generation g =
 		done;
 		print_newline();
 	end
-	done
+	done;
+	print_endline "---------------------";
 ;;
 
+(* Retourne le prochain état d'une cellule en fonction de son voisinage *)
+let rec next_state listeRules (n,e,s,o,cell) = match listeRules with
+	|[] -> cell
+	|a::t -> match a with 
+		|(a,b,c,d,f) when (a=n && b=e && c=s && d=o && f=cell) -> if cell = A then D else A
+		| _ -> next_state t (n,e,s,o,cell) 
+;;
+
+(* Retourne la prochaine génération g calculée à partir d'un automate et d'une génération *)
+let next_generation a (g : state array array) = 
+	let generationTemp = Array.make_matrix 7 7 A in 
+		for i=0 to sizeGrid-1 do
+		begin
+			for j=0 to sizeGrid-1 do
+			begin
+				let voisins = 
+					((if i-1 < 0 then g.(sizeGrid-1).(j) else g.(i-1).(j)),
+					(if j+1 > sizeGrid-1 then g.(i).(0) else g.(i).(j+1)),
+					(if i+1 > sizeGrid-1 then g.(0).(j) else g.(i+1).(j)),
+					(if j-1 < 0 then g.(i).(sizeGrid-1) else g.(i).(j-1)),
+					g.(i).(j))
+					in
+						generationTemp.(i).(j) <- next_state a voisins 
+			end
+			done
+		end
+		done;
+		generationTemp
+;;
+
+let rule1 = (A,A,A,A,A);;
+let rule2 = (A,A,A,A,D);;
+let rule3 = (A,A,A,D,A);;
+let rule4 = (A,A,A,D,D);;
+
+let automaton = rule1::rule2::rule3::rule4::[];;
+
+let generationZero = Array.make_matrix 7 7 A;; 
+setState generationZero 0 0 D;;
+setState generationZero 1 1 D;;
+setState generationZero 2 2 D;;
+setState generationZero 3 3 D;;
+setState generationZero 4 4 D;;
+setState generationZero 5 5 D;;
+setState generationZero 6 6 D;;
+
+let generationUne = next_generation automaton generationZero;;
+
 show_generation generationZero;;
+show_generation generationUne;;
